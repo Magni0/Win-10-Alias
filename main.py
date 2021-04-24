4a3d9c4384e5f14b30de2972840e66d703e3b856
Logan Harris <loganrodharris@gmail.com>
2021-04-24 20:27:33 +1000

    added argument parser description and help desc for arguments

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ class Alias:
 
     def __init__(self, args):
         self.args = args
+    
+    def run(self):
+        pass
 
     def create_alias(self):
         pass
@@ -18,12 +21,14 @@ class Alias:
 parser = ArgumentParser(
     formatter_class=RawDescriptionHelpFormatter,
     description="""
+    Allows aliases for specified paths the be created
+    speeding up cmd navigation.
     """
 )
 
-parser.add_argument("-c", "--create", action="store_true", help="")
-parser.add_argument("-p", "--path", type=str, help="")
-parser.add_argument("-a", "--alias", type=str, help="")
+parser.add_argument("-c", "--create", action="store_true", help="used to create a new alias (-p and -a flags must follow)")
+parser.add_argument("-p", "--path", type=str, help="argument is the absolute path that you wish to make a new alias")
+parser.add_argument("-a", "--alias", type=str, help="argument is the name of the new alias")
 
 args = parser.parse_args()
 alias = Alias(args)
