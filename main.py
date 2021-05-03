e9b94f9134fbb951c0232d3262e3bc832bcb5810
Logan Harris <loganrodharris@gmail.com>
2021-05-03 12:09:09 +1000

    Alias.remove_alias now working

--- main.py
+++ main.py
@@ -28,7 +28,18 @@ class Alias:
             file.write(dumps(aliases))
 
     def remove_alias(self):
-        pass
+        alias = self.args.alias
+
+        with open("alias.json", "r") as file:
+            try:
+                aliases = loads(file.read())
+            except:
+                raise Exception("No stored aliases")
+        
+        del aliases[f"{alias}"]
+
+        with open("alias.json", "w") as file:
+            file.write(dumps(aliases))
 
 parser = ArgumentParser(
     formatter_class=RawDescriptionHelpFormatter,
@@ -45,8 +56,9 @@ else:
     with open("alias.json", "x"): pass
 
 parser.add_argument("-c", "--create", action="store_true", help="used to create a new alias (-p and -a flags must follow)")
-parser.add_argument("-p", "--path", type=str, help="argument is the absolute path that you wish to make a new alias")
-parser.add_argument("-a", "--alias", type=str, help="argument is the name of the new alias")
+parser.add_argument("-p", "--path", type=str, help="argument is absolute path")
+parser.add_argument("-a", "--alias", type=str, help="argument is the name of alias")
+parser.add_argument("-r", "--remove", action="store_true", help="")
 
 args = parser.parse_args()
 alias = Alias(args)
