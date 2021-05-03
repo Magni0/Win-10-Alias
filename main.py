93cf65831f92f11596ee0c46b1a9b0847521e018
Logan Harris <loganrodharris@gmail.com>
2021-05-03 15:17:18 +1000

    module level control flow added for Alias.create_alias & Alias.remove_alias

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from os import chdir, path
+from os import chdir, path, getcwd
+from sys import argv
 from json import loads, dumps
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
@@ -8,7 +9,19 @@ class Alias:
         self.args = args
     
     def use_alias(self):
-        pass
+        alias = self.args.alias
+
+        with open("alias.json", "r") as file:
+            try:
+                aliases = loads(file.read())
+            except:
+                raise Exception("No stored aliases")
+        
+        if alias in aliases:
+            chdir(aliases[alias])
+            print(getcwd())
+        else:
+            raise Exception("invalid alias")
 
     def create_alias(self):
         # print(self.args)
@@ -26,6 +39,8 @@ class Alias:
 
         with open("alias.json", "w") as file:
             file.write(dumps(aliases))
+        
+        print(f"Created alias: {alias} for {path}")
 
     def remove_alias(self):
         alias = self.args.alias
@@ -36,10 +51,13 @@ class Alias:
             except:
                 raise Exception("No stored aliases")
         
+        path = aliases[f"{alias}"]
         del aliases[f"{alias}"]
 
         with open("alias.json", "w") as file:
             file.write(dumps(aliases))
+        
+        print(f"Removed alias: {alias} for {path}")
 
 parser = ArgumentParser(
     formatter_class=RawDescriptionHelpFormatter,
@@ -49,12 +67,6 @@ parser = ArgumentParser(
     """
 )
 
-# check if alias file exists if not creates file
-if path.isfile("alias.json"):
-    pass
-else:
-    with open("alias.json", "x"): pass
-
 parser.add_argument("-c", "--create", action="store_true", help="used to create a new alias (-p and -a flags must follow)")
 parser.add_argument("-p", "--path", type=str, help="argument is absolute path")
 parser.add_argument("-a", "--alias", type=str, help="argument is the name of alias")
@@ -62,3 +74,18 @@ parser.add_argument("-r", "--remove", action="store_true", help="")
 
 args = parser.parse_args()
 alias = Alias(args)
+
+# check if alias file exists if not creates file
+if path.isfile("alias.json"):
+    pass
+else:
+    with open("alias.json", "x"): pass
+
+if args.create == True:
+    alias.create_alias()
+
+elif args.remove == True:
+    alias.remove_alias()
+
+else:
+    alias.use_alias()
